d3032f742ef6b4e7a8f81c84afedca1cf73a178a
“VladSshy” <partizanjava@gmail.com>
2025-04-04 11:13:53 +0300

    Added test steps to TC1 and updated README

--- tests/searchTC1E2E.spec.ts
+++ tests/searchTC1E2E.spec.ts
@@ -19,21 +19,25 @@ test.describe('End-to-End search flow', () => {
     test('Search flow verify the search button , search result and search functionality', async ({
         page,
     }) => {
-        //verify we located on a main page where we can see the home page link, title, discord icon, gitHub icon, search
-        homePage.verifyHomePage();
-        homePage.verifygitHubIcon();
-        homePage.verifyDiscordLink();
-        homePage.verifysearchButton();
+        await test.step('Verify we located on a main page where we can see the home page link, title, discord icon, gitHub icon, search', async () => {
+            homePage.verifyHomePage();
+            homePage.verifygitHubIcon();
+            homePage.verifyDiscordLink();
+            homePage.verifysearchButton();
+        });
 
-        searchPage.verifySearchInput();
-        searchPage.verifySearchInputIcone();
-        //verify search funstionality we put docs and received proper data
-        searchPage.inputTxt('docs');
-        await searchPage.verifyLinksVisibility(linksToVerify);
+        await test.step('verify search funstionality we put docs and received proper data', async () => {
+            searchPage.verifySearchInput();
+            searchPage.verifySearchInputIcone();
+            //verify search funstionality we put docs and received proper data
+            searchPage.inputTxt('docs');
+            await searchPage.verifyLinksVisibility(linksToVerify);
+        });
 
-        //verify search functionality we click to the release note and was redirected to another endpoin
-        searchPage.searchSelect();
-        await page.waitForURL('**/release-notes');
-        await releaseNotes.verifyReleaseNotePage('Release notes');
+        await test.step('verify search functionality we click to the release note and was redirected to another endpoint', async () => {
+            searchPage.searchSelect();
+            await page.waitForURL('**/release-notes');
+            await releaseNotes.verifyReleaseNotePage('Release notes');
+        });
     });
 });
