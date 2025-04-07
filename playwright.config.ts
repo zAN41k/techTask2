56b4de0e0e4450ade9d0a61205adc2293e7bab3f
zAN41k <partizanjava@gmail.com>
2025-04-07 12:47:50 +0300

    Added allure for my project

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import {defineConfig, devices} from '@playwright/test';
+import allure from 'allure-playwright';
 
 /**
  * Read environment variables from file.
@@ -22,7 +23,7 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    reporter: [['list'], ['allure-playwright']],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
