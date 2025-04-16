f0b88a3773c54784341c4a306636f10f3a6e8886
zAN41k <partizanjava@gmail.com>
2025-04-16 10:35:06 +0300

    Added screenshot for debugging test

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
         baseURL: 'https://playwright.dev',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        screenshot: 'only-on-failure',
         trace: 'on-first-retry',
     },
 
--- tests/iframe.spec.ts
+++ tests/iframe.spec.ts
@@ -8,6 +8,6 @@ test('test', async ({page}) => {
     const frame = page.frameLocator('#iFrame1');
     await frame.locator('div.rsw-ce').fill('Hello there');
     expect(await frame.locator('div.rsw-ce').textContent()).toEqual(
-        'Hello there',
+        'Hello there2',
     );
 });
